684f8babd5bab9f96963c3e3abf3721c08ca46d2
Gloria Villa <hello.gloria@gmail.com>
2024-06-24 09:17:37 -0400

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Gloria Villa on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
